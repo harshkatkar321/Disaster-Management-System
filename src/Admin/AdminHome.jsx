7281957a1b0e407d5c6b25dc46466c3f7e642062
harshkatkar321 <harshkatkar54@gmail.com>
2025-07-29 10:25:46 +0530

    Clean remote repo to re-push with new structure

--- src/Admin/AdminHome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const AdminHome = () => {
-    return (
-        <div>
-            <h1>Admin Home </h1>
-        </div>
-    )
-}
--- src/Alerts/AlertCreate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const AlertCreate = () => {
-    return (
-        <div>
-            <h1>alert page</h1>
-        </div>
-    )
-}
